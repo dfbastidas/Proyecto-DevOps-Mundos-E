b34a476c2ab8ca53afea0c063463cf00dcde0509
Diego Bastidas <diegofernando150@gmail.com>
2025-04-09 12:16:10 -0500

    GitHub Actions: Levantar Infraestructura del k8s remoto con terraform y github actions

--- remote-k8s-host-control/02-instance.tf
+++ remote-k8s-host-control/02-instance.tf
@@ -4,11 +4,7 @@ resource "aws_instance" "remote-host-control" {
   instance_type = var.instance_type
 
   # ejecutar un script inmediatamente despues de crear la instancia EC2:
-  user_data = <<-EOF
-                    #!/bin/bash
-                    sudo apt-get update
-                    sudo apt-get install -y apache2
-                EOF
+  user_data = "${file("set-k8s-host-control.sh")}"
 
   # Asigna la llave que hemos creado a la instancia:
   key_name = aws_key_pair.remote-host-control-ssh.key_name
---
+++ set-k8s-host-control.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install AWS CLI
+sudo apt-get install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
